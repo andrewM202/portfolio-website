6367312a9d9767c4a410cb347f7b89672dbad8cb
Andrew Matt <andrewm1246@gmail.com>
2021-07-10 17:31:54 -0400

    Attempt to make next and prev buttons work on mobile

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -8,19 +8,4 @@ $(document).ready(function () {
         $("header h1").hide().fadeIn(1500);
     }
 
-    // Make buttons fill in white on mobile
-    $(".carousel-control-next").click(function() {
-        $(".carousel-control-next").css('background', '#EFEFEF'); // White
-
-        setTimeout(function() {
-            $(".carousel-control-next").css('background', 'rgb(239, 239, 239)'); // Gray
-        }, 1000);
-    });
-    $(".carousel-control-prev").click(function() {
-        $(".carousel-control-prev").css('background', '#EFEFEF'); // White
-
-        setTimeout(function() {
-            $(".carousel-control-prev").css('background', 'rgb(239, 239, 239)'); // Gray
-        }, 1000);
-    });
 });
