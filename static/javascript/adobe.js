bde77591b59916609b3dfa1fc461aebda977db80
Andrew Matt <andrewm1246@gmail.com>
2021-09-11 22:07:24 -0400

    Added inline adobe pdf support

---
+++ static/javascript/adobe.js
@@ -0,0 +1,41 @@
+// From adobe github: https://github.com/adobe/pdf-embed-api-samples/blob/master/In-Line%20Embed%20Mode/index.js
+/* Pass the embed mode option here */
+const viewerConfig = {
+    embedMode: "IN_LINE"
+};
+
+/* Wait for Adobe Document Services PDF Embed API to be ready */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "c17f9f743632475dbfeee2a78ff3409b",
+        /* Pass the div id in which PDF should be rendered */
+        divId: "adobe-dc-view",
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: url,
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "AndrewMattResume.pdf"
+        }
+    }, viewerConfig);
+});
