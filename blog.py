aafcf85c81fb61be20221b390682ab6828d9e1a8
Andrew <andrewm1246@gmail.com>
2021-11-26 12:33:00 -0500

    Removed flask_mongoengine and replaced with mongoengine. Images for blog now stored in database and dynamically loaded into blog folder when blog route is called. This is the solution to Heroku only temporarily storing dynamic content in folders.

--- blog.py
+++ blog.py
@@ -1,22 +1,38 @@
-from flask import Blueprint, request, Flask, render_template, redirect, jsonify 
+from flask import Blueprint, request, Flask, render_template, redirect, jsonify
 from flask_login import current_user
-from models import Quickchat, Article, db
+from models import Quickchat, Article
 from datetime import date, datetime
 from resume import allowed_file
 from werkzeug.utils import secure_filename
 import string, random, os
 from app import app
+# For some reason need this for saving DB images
+from PIL import Image
 
 bp = Blueprint("blog", __name__)
+
+def render_images():
+    """ Initializes the images from database
+    into local storage """
+    articles = Article.objects()
+    for art in articles:
+        # Save image to directory if it 
+        # is not already in directory
+        # path is path to file
+        path = os.path.join(app.config['BLOG_FOLDER'], art.coverimage_name)
+        if not os.path.isfile(path):
+            with Image.open(art.coverimage) as im:
+                im.save(os.path.join(app.config['BLOG_FOLDER'] + art.coverimage_name), 'JPEG')
 
 @bp.route("/blog")
 def blogroute():
     """ Route for blog """
     articles = Article.objects()
-    # print(dir(articles))
-    # print(articles[0].date)
-    # articles[0].date = datetime.strptime(str(articles[0].date), "%Y-%m-%d").strftime("%b %d, %Y")
-    # print(datetime.strptime(str(articles[0].date), "%Y-%m-%d").strftime("%b %d, %Y"))
+    # In Heroku, dynamically added files into a folder are not permanent,
+    # and are lost upon the dynamos stopping or updating. So, we need 
+    # to load all of our files into the blog folder from out mongodb database
+    # which is what render_images() does
+    render_images()
     return render_template("blog.html", articles=articles)
 
 @bp.route("/blog-editor")
@@ -52,29 +68,23 @@ def seeArticle(id):
     adobe_id = os.environ.get('ADOBE_ID')
     return render_template("article.html", article=article, adobe_id=adobe_id)
 
-
 @bp.route("/process-article", methods=['POST'])
 def processArticle():
     """ Process a blog article's content """
     if current_user.is_authenticated:
         if request.method == 'POST':
+            # Create mongoengine connection 
+            data = Article()
+
             articleContent = request.form['ckeditor']
             articleTitle = request.form['title'].title()
             articleDesciption = request.form['description'] 
 
-            article_pdf_filename = ""
+            article_pdf_filename = None
             if request.files['uploaded-article'] and (articleContent is None or articleContent == ""):
                 article_pdf = request.files['uploaded-article']
                 if article_pdf and allowed_file(article_pdf.filename):
-                    # Extension of file
-                    extension = article_pdf.filename.rsplit('.', 1)[1].lower()
-                    # give the file a random name
-                    letters = string.ascii_letters
-                    digits = string.digits
-                    article_pdf_filename = ''.join(random.choice(letters + digits) for i in range(20)) + "." + extension
-                    article_pdf_filename = secure_filename(article_pdf_filename)
-                    # save file to specified directory
-                    article_pdf.save(os.path.join(app.config['BLOG_FOLDER'], article_pdf_filename))
+                    date.uploaded_content=article_pdf
                 else:
                     return "Not allowed file type"
 
@@ -87,20 +97,19 @@ def processArticle():
                 digits = string.digits
                 filename = ''.join(random.choice(letters + digits) for i in range(20)) + "." + extension
                 filename = secure_filename(filename)
+                data.coverimage.replace(file, filename=filename)
+                data.coverimage_name = filename + '.' + extension
                 # save file to specified directory
                 file.save(os.path.join(app.config['BLOG_FOLDER'], filename))
             else:
                 return "Invalid File"
 
-            data = Article(
-                title = articleTitle,
-                content = articleContent,
-                uploaded_content = article_pdf_filename,
-                coverimage = filename, # The name of the image
-                articledesc = articleDesciption,
-                date = date.today().strftime("%b %d, %Y"),
-                formatted_date = str(date.today().strftime("%b %d, %Y"))
-            )
+            data.title = articleTitle
+            data.content = articleContent
+            data.articledesc = articleDesciption
+            data.date = date.today().strftime("%b %d, %Y")
+            data.formatted_date = str(date.today().strftime("%b %d, %Y"))
+
             data.save()
 
             return redirect('/blog-editor')
